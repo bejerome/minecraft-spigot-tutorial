a80de69b3bfcb0ea80ade8dd6f2af0dc2fe2780d
Ben Jerome <bjerome43@massmutual.com>
2020-07-30 10:25:04 -0400

    added command

--- src/com/ben/firstplugin/Main.java
+++ src/com/ben/firstplugin/Main.java
@@ -25,21 +25,29 @@ public class Main extends JavaPlugin implements Listener{
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equals("hello")) {
-//			check if the sender is an instance of the player object
+			
+			//check if the sender is an instance of the player object
+			
 			if (sender instanceof Player) {
+				
 				// create an instance of player
+				
 				Player player = (Player) sender;
-				// send message
-				player.sendMessage(ChatColor.DARK_RED + "Hello, " + ChatColor.GREEN + player.getName() + ChatColor.DARK_GRAY + " You Fuzzy Head");
-				// set player health to full
+				
+			// send message
+				
+			player.sendMessage(ChatColor.DARK_RED + "Hello, " + ChatColor.GREEN + player.getName() + ChatColor.DARK_GRAY + " You Fuzzy Head");
+			
+			// set player health to full
+			
 				player.setHealth(20.0);
 			} else {
-				System.out.println("You cannot use this command through console!");
+			System.out.println("You cannot use this command through console!");
 			}
 			
 		}
 		return false;
 		
-	}
+	}	
 
 }
