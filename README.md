e1087a49f8e865de594bcf00c1dd48c2da6d9b7b
Ben Jerome <bjerome43@massmutual.com>
2020-07-30 12:18:15 -0400

    added command args

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # minecraft-spigot-tutorial
+
+https://hub.spigotmc.org/javadocs/spigot/org/bukkit/entity/Entity.html
--- src/com/ben/firstplugin/Main.java
+++ src/com/ben/firstplugin/Main.java
@@ -3,17 +3,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class Main extends JavaPlugin {
+	
 	@Override
+	
 	public void onEnable() {
 		
 		System.out.println("First Plugin worked!");
 		
-		getCommand("heal").setExecutor(new HealCommand());
-		getCommand("time").setExecutor(new HealCommand());
+		getCommand("number").setExecutor(new CustomCommand());
+		
 		
 	}
 	
-
-
-
 }
